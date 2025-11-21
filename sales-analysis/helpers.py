17ba8cdbff64e4070df3abbe7dd3aab3b5b6e3ee
Rammi465 <ramarao.465@gmail.com>
2025-11-21 14:34:56 -0600

    Initial commit

---
+++ sales-analysis/helpers.py
@@ -0,0 +1,14 @@
+def calculate_total(quantity, price):
+    """Calculate Total for Single item"""
+    return quantity * price
+
+def format_currency(amount):
+    """Format number as currency"""
+    return f"{amount:,.2f}"
+
+
+
+
+
+
+
